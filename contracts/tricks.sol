a0be4e07fcbd4fb9855d4943cc682a42e48a6a3c
Baptoo <baptiste.cpry@gmail.com>
2023-05-15 14:27:24 +0200

    tricks.sol update (propreties & attributes)

--- contracts/tricks.sol
+++ contracts/tricks.sol
@@ -1,9 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
@@ -19,7 +18,7 @@ contract Tricks is ERC721Enumerable, Ownable {
     string public baseURI;
     address public serverAddress;
     uint public mintPrice = 390;
-    mapping(uint => address) public owners;
+    address[] owners;
     mapping(uint => uint[]) public attributes;
     mapping(uint => uint[]) public privileges;
 
@@ -29,22 +28,37 @@ contract Tricks is ERC721Enumerable, Ownable {
     AggregatorV3Interface internal priceFeed_ETHUSD;
     AggregatorV3Interface internal priceFeed_EURUSD;
 
+    struct NFTAttributes {
+        uint256 femme;
+        uint256 handi;
+        uint256 homme;
+        uint256 lancerBalayer;
+        uint256 tricks2;
+        uint256 tricks3;
+        uint256 tricks4;
+        uint256 tricks1;
+    }
+
+    struct NFTPrivileges {
+        uint256 bronze;
+        uint256 argent;
+        uint256 or;
+        uint256 platine;
+    }
+
+
+    mapping(uint256 => NFTAttributes) private _tokenAttributes;
+    mapping(uint256 => NFTPrivileges) private _tokenPrivileges;
+
+
     constructor(string memory _name, string memory _symbol, string memory _baseURI) ERC721(_name, _symbol) {
         setBaseURI(_baseURI);
         priceFeed_ETHUSD = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
         priceFeed_EURUSD = AggregatorV3Interface(0xb49f677943BC038e9857d61E7d053CaA2C1734C1);
     }
 
-    //function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, AccessControl) returns (bool) {
-    //   return super.supportsInterface(interfaceId);
-    // }
-
     function setBaseURI(string memory _newBaseURI) public onlyOwner {
         baseURI = _newBaseURI;
-    }
-
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseURI;
     }
 
     function pause() public onlyOwner {
@@ -56,24 +70,12 @@ contract Tricks is ERC721Enumerable, Ownable {
     }
 
     function getLatestPriceEURUSD() public view returns (int) {
-        (
-        uint80 roundID,
-        int price,
-        uint startedAt,
-        uint timeStamp,
-        uint80 answeredInRound
-        ) = priceFeed_EURUSD.latestRoundData();
+        (, int256 price, , , ) = priceFeed_EURUSD.latestRoundData();
         return price;
     }
 
     function getLatestPriceETHUSD() public view returns (int) {
-        (
-        uint80 roundID,
-        int price,
-        uint startedAt,
-        uint timeStamp,
-        uint80 answeredInRound
-        ) = priceFeed_ETHUSD.latestRoundData();
+        (, int256 price, , , ) = priceFeed_ETHUSD.latestRoundData();
         return price;
     }
 
@@ -88,32 +90,107 @@ contract Tricks is ERC721Enumerable, Ownable {
         uint256 priceEth = (priceEur * uint256(priceEurUsd)) / uint256(priceEthUsd);
         return priceEth;
     }
+    /**
+        function NFTMint(address _to, string memory _NFTURI) public payable onlyOwner {
 
-    function NFTMint(address _to, string memory _baseURI) public payable onlyOwner {
+            uint256 mintPriceInEuros = 390;
+            uint256 mintPriceInEther = convertEurToEth(mintPriceInEuros);
 
-        uint256 mintPriceInEuros = 390;
-        uint256 mintPriceInEther = convertEurToEth(mintPriceInEuros);
+            require(msg.value >= mintPriceInEther, "Insufficient funds");
 
-        require(msg.value >= mintPriceInEther, "Insufficient funds");
+            require(!paused);
 
-        require(!paused);
+            uint256 supply = totalSupply();
+            require(supply <= maxSupply);
 
-        uint256 supply = totalSupply();
-        require(supply <= maxSupply);
+            // Mince le NFT
+            uint256 tokenId = _tokenIdCounter.current();
+            _tokenIdCounter.increment();
+            _safeMint(_to, tokenId);
 
-        // Mince le NFT
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(_to, tokenId);
+        }
+    */
+
+
+
+
+    //en fonction de la valeur "attribute" existante dans le fichier .json de chaque NFT
+    function setTokenAttributesByURI(string memory _tokenURI) internal onlyOwner {
+
+        uint256 tokenId = getTokenIdByURI(_tokenURI);
+        require(_exists(tokenId), "ERC721Metadata: L'URI associee au token n'existe pas. Le token n'existe donc pas.");
+
+        // Parcourir les attributs dans le fichier JSON correspondant a l'URI du token
+        //Stocker les data dans la struct NFTAttributes
+
+        NFTAttributes memory newAttributes;
+
+        _tokenAttributes[tokenId] = newAttributes;
+    }
+
+
+    function getTokenAttributesByURI(string memory _tokenURI) public view returns (NFTAttributes memory) {
+
+        uint256 tokenId = getTokenIdByURI(_tokenURI);
+        require(_exists(tokenId), "ERC721Metadata: L'URI associee au token n'existe pas. Le token n'existe donc pas.");
+
+        return _tokenAttributes[tokenId];
 
     }
+
+
+    //en fonction de la valeur "privileges" existante dans le fichier .json de chaque NFT
+    function setTokenPrivilegesByURI(string memory _tokenURI) internal onlyOwner {
+
+        uint256 tokenId = getTokenIdByURI(_tokenURI);
+        require(_exists(tokenId), "ERC721Metadata: L'URI associee au token n'existe pas. Le token n'existe donc pas.");
+
+        // Parcourir les privileges dans le fichier JSON correspondant a l'URI du token
+        //Stocker les data dans la struct NFTPrivileges
+
+        NFTPrivileges memory newPrivileges;
+
+        _tokenPrivileges[tokenId] = newPrivileges;
+    }
+
+
+    function getTokenPrivilegesByURI(string memory _tokenURI) public view returns (NFTPrivileges memory) {
+
+        // Parcourir les privileges dans le fichier JSON correspondant a l'URI du token
+        //Stocker les data dans le tableau tokenPrivileges
+        //Retourner le tableau
+
+        uint256 tokenId = getTokenIdByURI(_tokenURI);
+        require(_exists(tokenId), "ERC721Metadata: L'URI associee au token n'existe pas. Le token n'existe donc pas.");
+
+        return _tokenPrivileges[tokenId];
+    }
+
+
+
+
 
     function tokenURI(uint256 tokenId) public view virtual override(ERC721) returns (string memory) {
         require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
         return string(abi.encodePacked(baseURI, Strings.toString(tokenId), ".json"));
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual   { }
+    function getTokenIdByURI(string memory _tokenURI) public view returns (uint256) {
+        uint256 totalTokens = totalSupply();
+        for (uint256 tokenId = 0; tokenId < totalTokens; tokenId++) {
+            if (compareStrings(_tokenURI, tokenURI(tokenId))) {
+                return tokenId;
+            }
+        }
+        revert("Le token n'existe pas pour l'URI donnee en parametre");
+    }
+
+    function compareStrings(string memory a, string memory b) internal pure returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    }
+
+
+    //function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual   { }
 
 
     function _burn(uint256 tokenId) internal virtual override(ERC721) {
@@ -123,4 +200,37 @@ contract Tricks is ERC721Enumerable, Ownable {
 
         super._burn(tokenId);
     }
+
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal virtual {
+        super._beforeTokenTransfer(from, to, tokenId, 1); //batchSize specifie combien de jetons sont transferes
+        // dans un seul lot. Ici on transfere un seul token a la fois, donc la valeur de batchSize est 1.
+
+        require(to != address(0), "ERC721: transaction a l'adresse 0x");
+    }
+
+    /// MINT ///
+    function NFTMint(address _to, string memory _NFTURI) public payable onlyOwner {
+        uint256 mintPriceInEuros = 390;
+        uint256 mintPriceInEther = convertEurToEth(mintPriceInEuros);
+
+        require(msg.value >= mintPriceInEther, "Insufficient funds");
+        require(!paused);
+
+        uint256 supply = totalSupply();
+        require(supply < maxSupply);
+
+        // Mint le NFT
+        uint256 tokenId = getTokenIdByURI(_NFTURI);
+        //verification avec la fonction _beforeTokenTransfer
+        _beforeTokenTransfer(address(0), _to, tokenId);
+
+        //Mint
+        _safeMint(_to, tokenId);
+        owners.push(_to);
+
+        setTokenAttributesByURI(_NFTURI);
+        setTokenPrivilegesByURI(_NFTURI);
+    }
 }
+
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,8 @@
 const hre = require("hardhat");
 
 async function main() {
-    const TricksContract = await hre.ethers.getContractFactory("tricks");
-    const tricksContract = await TricksContract.deploy();
+    const TricksContract = await hre.ethers.getContractFactory("Tricks");
+    const tricksContract = await TricksContract.deploy("LesRemparts", "TRKS","ipfs://bafybeibnsoufr2renqzsh347nrx54wcubt5lgkeivez63xvivplfwhtpym/metadata.json");
     await tricksContract.deployed();
     console.log("Tricks contract deployed to:", tricksContract.address);
 }
