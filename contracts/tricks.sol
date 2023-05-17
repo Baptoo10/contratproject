ba8ff1c78de609dc2c29e5235c72fc1baacedd55
Baptoo <baptiste.cpry@gmail.com>
2023-05-17 16:34:21 +0200

    tricks.sol update

--- contracts/tricks.sol
+++ contracts/tricks.sol
@@ -285,9 +285,9 @@ contract Tricks is ERC721Enumerable, Ownable {
     }
 
 
-    function compareStrings(string memory a, string memory b) internal pure returns (bool) {
-        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
-    }
+    // function compareStrings(string memory a, string memory b) internal pure returns (bool) {
+    //   return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
+    //}
 
 
 
